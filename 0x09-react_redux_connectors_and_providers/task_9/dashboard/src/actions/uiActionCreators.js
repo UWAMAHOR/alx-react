c728aabee90e019bd4e82fc7a747bd96fbe8aa26
UWAMAHOR <uwamahoropascaline11@gmail.com>
2024-07-03 13:52:28 +0000

    task_9

---
+++ 0x09-react_redux_connectors_and_providers/task_9/dashboard/src/actions/uiActionCreators.js
@@ -0,0 +1,27 @@
+import {
+  LOGIN,
+  LOGOUT,
+  DISPLAY_NOTIFICATION_DRAWER,
+  HIDE_NOTIFICATION_DRAWER,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+} from "./uiActionTypes";
+
+import "node-fetch";
+
+export const login = (email, password) => {
+  return {
+    type: LOGIN,
+    user: { email, password },
+  };
+};
+
+export const boundLogin = (email, password) => dispatch(login(email, password));
+
+export const logout = () => {
+  return {
+    type: LOGOUT,
+  };
+};
+
+export const boundLogout = () => dispatch(logout());
